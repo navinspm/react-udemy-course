b4bce4e3ec69a0348c138ccb90f2e9788ee30e87
sudha <nkrsudha@gmail.com>
2020-05-01 07:25:29 +0530

    mo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import classes from './App.module.css';
 import Person from './Person/Person';
 
 
@@ -46,8 +46,8 @@ class App extends Component {
 
   render() {
     
-
    let persons = null;
+   let btnClass = [classes.Button];
 
    if (this.state.showPersons) {
       persons = (
@@ -63,23 +63,23 @@ class App extends Component {
             })}
           </div>
       )
-     
+     btnClass.push(classes.Red);
    }
    
-   let classes = [];
+   let assignedClasses = [];
    if(this.state.persons.length <= 2){
-    classes.push('red');
+    assignedClasses.push(classes.red);
    }
 
    if(this.state.persons.length <= 1){
-    classes.push('bold');
+    assignedClasses.push(classes.bold);
    }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi, Im a React App</h1>
-          <p className={classes.join(' ')}>This is really working</p>
-          <button className='button'
+          <p className={assignedClasses.join(' ')}>This is really working</p>
+          <button className={btnClass.join(' ')}
             onClick={this.togglePersonsHandler}>Toggle Persons
           </button>  
           {persons}
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,30 +1,16 @@
 import React from 'react';
-//import './Person.css';
-import styled from 'styled-components';
-
-const StyledDiv = styled.div`
-		width:60%;
-		margin: auto;
-		border: 1px solid #eee;
-		box-shadow: 0 2px 3px #ccc;
-		padding: 16px;
-		text-align: center;
+import styles from './Person.module.css'
 
 
-		@media(min-width: 500px){
-		  width: 450px;
-		}
-		`;
 
 const person = (props) => {  
 	
    return (
-	   	 //<div className="Person" style={style}> 
-	        <StyledDiv>
+	   	 <div className="Person" > 
 	   	     <p onClick={props.click}>Im {props.name} and I am {props.age} years old</p>
 	   	     <p>{props.children}</p>
 	   	     <input type="text" onChange={props.changed} value={props.name}/>
-	   	 </StyledDiv>
+	   	 </div>
 	   )   
 };
 
