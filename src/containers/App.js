ff735a104b8de22efdf44d2f0ec6229881346377
Sudha <nkr.sudha@gmail.com>
2020-05-12 10:21:24 +0530

    added react hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,38 @@
 import React, {Component} from 'react';
 import classes from './App.module.css';
-import Person from '../components/Persons/Person/Person';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    console.log('[App.js] constructor');
+  }
   state  = {
     persons: [
       {id:'asfa1', name: 'Max', age: 28},
       {id:'wafd3', name: 'Manu', age: 29},
       {id:'ffdf4', name: 'Suvin', age: 28}
     ],
-    otherState: 'some other value',
-    showPersons: false
+    otherState: 'some other values',
+    showPersons: false,
+    showCockpit: true
   };
+ 
+  static getDerivedStateFromProps(props,state){
+    console.log('[App.js] getDerivedStateFromProps',props);
+    return state;
+  }
 
+  // componentWillMount(){
+  // console.log('[App.js] componentWillMount');
+  // }
+
+  componentDidMount(){
+    console.log('[App.js] componentDidMount');
+  }
+
+  
 
   nameChangedHandler = ( event , id) => {
     const personIndex = this.state.persons.findIndex(p => {
@@ -47,8 +65,8 @@ class App extends Component {
 
 
   render() {
-    
-   let persons = null;
+    console.log('[App.js] render'); 
+    let persons = null;
 
    if (this.state.showPersons) {
       persons =
@@ -59,10 +77,19 @@ class App extends Component {
    
     return (
         <div className={classes.App}>
+          <button 
+            onClick={()=>{
+            this.setState({showCockpit: false})
+          }}>Remove Cockpit</button>
+
+          {this.state.showCockpit ? (
           <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}/>
+          personsLength={this.state.persons.length}
+          clicked={this.togglePersonsHandler}
+          />
+          ) :  null }
           {persons}
         </div>
     );
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App appTitle="Person Manger"/>
   </React.StrictMode>,
   document.getElementById('root')
 );
